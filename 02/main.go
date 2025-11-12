64477c4b0e2a1d516428a73744734ef21901ecff
Zak Stroud <ztstroud@gmail.com>
2025-11-11 20:29:03 -0800

    feat(02): parse values from reports

--- 02/main.go
+++ 02/main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func parseReport(report string) ([]int, error) {
-	return make([]int, 0), nil
+	strVals := strings.Fields(report)
+	vals := make([]int, 0, len(strVals))
+
+	for _, strVal := range(strVals) {
+		val, err := strconv.Atoi(strVal)
+		if err != nil {
+			return nil, err
+		}
+
+		vals = append(vals, val)
+	}
+
+	return vals, nil
 }
 
--- 02/main_test.go
+++ 02/main_test.go
@@ -19,3 +19,31 @@ func TestParseReportEmpty(t *testing.T) {
 	}
 }
 
+func TestParseReportSingle(t *testing.T) {
+	input := "5"
+	expected := []int{5}
+	actual, err := parseReport(input)
+
+	if err != nil {
+		t.Errorf("Got an error: %v\n", err)
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v to equal %v\n", actual, expected)
+	}
+}
+
+func TestParseReportMany(t *testing.T) {
+	input := "5 2 4 3 1"
+	expected := []int{5, 2, 4, 3, 1}
+	actual, err := parseReport(input)
+
+	if err != nil {
+		t.Errorf("Got an error: %v\n", err)
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v to equal %v\n", actual, expected)
+	}
+}
+
