596018faeed5ddf03f10a2b11f3407669bb795f6
Zak Stroud <ztstroud@gmail.com>
2025-11-12 20:08:09 -0800

    test(03): add basic tests
    These new tests cover summing the products, and check for some simple
    garbage configurations.

--- 03/main_test.go
+++ 03/main_test.go
@@ -14,3 +14,43 @@ func TestSimple(t *testing.T) {
 	}
 }
 
+func TestMulti(t *testing.T) {
+	input := "mul(2,3)mul(1,4)mul(2,2)"
+	expected := 14
+	actual := sumMuls(input)
+
+	if expected != actual {
+		t.Errorf("Expected %v to be %v", actual, expected)
+	}
+}
+
+func TestGarbageExternal(t *testing.T) {
+	input := "^)|mmul(1,1)x8("
+	expected := 1
+	actual := sumMuls(input)
+
+	if expected != actual {
+		t.Errorf("Expected %v to be %v", actual, expected)
+	}
+}
+
+func TestGarbageName(t *testing.T) {
+	input := "mXul(1,1)"
+	expected := 0
+	actual := sumMuls(input)
+
+	if expected != actual {
+		t.Errorf("Expected %v to be %v", actual, expected)
+	}
+}
+
+func TestGarbageArgument(t *testing.T) {
+	input := "mul(1X,1)"
+	expected := 0
+	actual := sumMuls(input)
+
+	if expected != actual {
+		t.Errorf("Expected %v to be %v", actual, expected)
+	}
+}
+
