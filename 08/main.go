4b09c3f3df501de50aaec88497b3b81f1b98e213
Zak Stroud <ztstroud@gmail.com>
2025-11-16 15:24:12 -0800

    feat(08): add getAntennaGroups
    The groups will give you a smaller search space when looking at pairs of
    antennas.

--- 08/main.go
+++ 08/main.go
@@ -1,6 +1,10 @@
 package main
 
 type City [][]byte
+type Position struct{ x, y int }
+type AntennaGroups map[byte][]Position
+
+const EMPTY = 0
 
 func parseCity(src []string) City {
 	city := make([][]byte, len(src))
@@ -9,10 +13,34 @@ func parseCity(src []string) City {
 		for x, char := range []byte(line) {
 			if char != '.' {
 				city[y][x] = char
+			} else {
+				city[y][x] = EMPTY
 			}
 		}
 	}
 
 	return city
+}
+
+func getAntennaGroups(city City) AntennaGroups {
+	groups := make(AntennaGroups)
+	for y := range city {
+		for x := range city[y] {
+			antenna := city[y][x]
+			if antenna == EMPTY {
+				continue
+			}
+
+			group, ok := groups[antenna]
+			if !ok {
+				group = make([]Position, 0)
+			}
+
+			group = append(group, Position{ x, y })
+			groups[antenna] = group
+		}
+	}
+
+	return groups
 }
 
--- 08/main_test.go
+++ 08/main_test.go
@@ -27,3 +27,23 @@ func TestParseCity(t *testing.T) {
 	}
 }
 
+func TestGetAntennaGroups(t *testing.T) {
+	input := []string{
+		".0..",
+		"....",
+		"..A.",
+		"...A",
+	}
+
+	groups := getAntennaGroups(parseCity(input))
+
+	expectedGroups := AntennaGroups{
+		'0': { { 1, 0 } },
+		'A': { { 2, 2 }, { 3, 3 } },
+	}
+
+	if !reflect.DeepEqual(groups, expectedGroups) {
+		t.Errorf("Expected %v to be %v", groups, expectedGroups)
+	}
+}
+
